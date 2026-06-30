6ef06725b41401b2afb12c6d9adca96025f5e4f3
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2026-07-01 01:00:42 +0600

    rbac implemented

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiAuth } from '@shared/decorators/api-auth.decorator';
+import { Roles } from '@shared/decorators/roles.decorator';
+import { UserRole } from '@core/database/common/enums';
 
 @Controller('user')
 export class UserController {
@@ -12,6 +15,8 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
+  @ApiAuth()
+  @Roles(UserRole.ADMIN)
   @Get()
   findAll() {
     return this.userService.findAll();
